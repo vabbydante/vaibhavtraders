fba2746259754e6995cf72d8c8e5b1edb80d03d9
Vaibhav <rohitvaibhavgupta@gmail.com>
2020-08-21 21:46:01 +0530

    changes

--- app/src/main/java/com/vaibhav/vaibhavtraders/OrderViewActivity.java
+++ app/src/main/java/com/vaibhav/vaibhavtraders/OrderViewActivity.java
@@ -2,7 +2,15 @@ package com.vaibhav.vaibhavtraders;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioAttributes;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -12,6 +20,8 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Locale;
 
 public class OrderViewActivity extends AppCompatActivity {
 
@@ -39,6 +49,8 @@ public class OrderViewActivity extends AppCompatActivity {
                 for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
                     OrderModel orderModel = documentSnapshot.toObject(OrderModel.class);
 
+                    sendNotification();
+
                     String date = orderModel.getDate();
                     String store = orderModel.getStore();
                     String orders = orderModel.getOrders();
@@ -48,5 +60,27 @@ public class OrderViewActivity extends AppCompatActivity {
                 viewOrders.setText(dataorder);
             }
         });
+    }
+
+    private void sendNotification() {
+        long[] v = {100, 420, 150, 420};
+        Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Intent intent = new Intent(this, OrderViewActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.logo)
+                .setContentTitle("You have new orders!")
+                .setContentText("Tap to view incoming orders")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                //.setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setSound(uri)
+                .setVibrate(v)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(001, builder.build());
     }
 }
